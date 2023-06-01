6431c9a4a1194c4a49445390b3162bd62177e28e
faiz-webdev <faijuddin@tothenew.com>
2023-06-01 11:39:47 +0530

    setup routing in react done

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,10 @@ const express = require('express')
 connectToMongo();
 
 const app = express()
-const port = 3000
+const port = 5000
+
+app.use('/api/auth', require('./routes/auth'));
+// app.use('/api/notes', require('./routes/notes'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <NavBar
+          title="iNotebook"
+        />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </Router>
+      {/* <NavBar />
+      <Home /> */}
+    </>
   );
 }
 
