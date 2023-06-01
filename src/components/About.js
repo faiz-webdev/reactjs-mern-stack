ec75903a35b04beadc003b0a3176b2a67c21f26d
faiz-webdev <faijuddin@tothenew.com>
2023-06-01 12:45:36 +0530

    implementd useLocation hooks for current active page opend

--- src/components/About.js
+++ src/components/About.js
@@ -2,15 +2,9 @@ import React, { useContext, useEffect } from "react";
 import noteContext from "../context/notes/noteContext";
 
 function About() {
-  const a = useContext(noteContext);
-  useEffect(() => {
-    a.update();
-    // eslint-disable-next-line
-  }, []);
-
   return (
     <div>
-      About {a.state.name} and class {a.state.class}
+      About
     </div>
   );
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,24 +2,8 @@ import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const s1 = {
-    name: "Test",
-    class: "10",
-  };
-  const [state, setState] = useState(s1);
-  const update = () => {
-    setTimeout(() => {
-      setState({
-        name: "test updated",
-        class: "12",
-      });
-    }, 1000);
-  };
-
   return (
-    <NoteContext.Provider value={{ state, update }}>
-      {props.children}
-    </NoteContext.Provider>
+    <NoteContext.Provider value={{}}>{props.children}</NoteContext.Provider>
   );
 };
 
